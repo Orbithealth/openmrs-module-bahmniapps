11bb7cc66ebffd29b8b349ebe86d70fc14f5bbac
betsegaw_degefe <betsegawyes@gmail.com>
2020-08-07 14:52:56 +0300

    make date of birth ethiopian calender

--- ui/app/common/util/dateUtil.js
+++ ui/app/common/util/dateUtil.js
@@ -1,6 +1,27 @@
 'use strict';
 
 Bahmni.Common.Util.DateUtil = {
+    months: ["መስከረም", "ጥቅምት", "ኅዳር", "ታኅሣሥ", "ጥር", "የካቲት", "መጋቢት", "ሚያዝያ", "ግንቦት", "ሰኔ", "ሐምሌ", "ነሐሴ", "ጳጉሜ"],
+    // function used to convert from gregorian date to ethiopian date with textulal month names
+    gregToEthMonthName: function (gregdate) {
+        try {
+            if (gregdate == "" || gregdate == null) return null;
+            var dmy;
+            dmy = gregdate.split("-");// first try the - separator
+            if (dmy.length != 3) dmy = gregdate.split("/");// then try the / separator
+            if (dmy.length == 3) {
+                var appdate = jQuery.calendars.instance('gregorian').newDate(parseInt(dmy[2], 10), parseInt(dmy[0], 10), parseInt(dmy[1], 10));
+                var jd = jQuery.calendars.instance('gregorian').toJD(appdate);
+                var appdateet = jQuery.calendars.instance('ethiopian').fromJD(jd);
+                // var appdateetstr = jQuery.calendars.instance('ethiopian').formatDate('dd/mmm/yyyy', appdateet);
+                return this.months[appdateet.month() - 1] + "/" + appdateet.day() + "/" + appdateet.year();
+            }
+            else return null;
+        }
+        catch (e) {
+            return null;
+        }
+    },
     diffInDays: function (dateFrom, dateTo) {
         return Math.floor((this.parse(dateTo) - this.parse(dateFrom)) / (60 * 1000 * 60 * 24));
     },
