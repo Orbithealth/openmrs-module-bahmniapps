e9d7cd59183fc2c37029b64af6ec8264964d2509
betsegaw_degefe <betsegawyes@gmail.com>
2020-08-25 14:38:15 +0300

    patient document upload date related issue solved

--- ui/app/document-upload/init.js
+++ ui/app/document-upload/init.js
@@ -4,5 +4,5 @@ var Bahmni = Bahmni || {};
 Bahmni.DocumentUpload = Bahmni.DocumentUpload || {};
 
 angular.module('opd.documentupload', ['bahmni.common.patient', 'bahmni.common.config', 'bahmni.common.domain',
-    'bahmni.common.gallery', 'bahmni.common.logging']);
+    'bahmni.common.gallery', 'bahmni.common.logging', 'bahmni.common.queue', 'bahmni.common.ethiopianDateSelector']);
 
